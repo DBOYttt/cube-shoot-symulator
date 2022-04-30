d7392269237a9bbf78eb7a3d90318e0be2d752cc
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-04-30 18:07:12 +0200

    first version

---
+++ cube.py
@@ -0,0 +1,22 @@
+import imp
+
+
+import random
+
+print('hi im tobi beta function')
+
+
+
+while 1:
+
+    x = input('can we start?(yes or no):')
+
+    if x == 'yes':
+        print(random.randint(1, 6))
+
+    else:
+        break   
+
+
+
+
